0d27669cf1ed5f8f71d9c19e71df0510c0d1d8fa
Christian <vargasc475@gmail.com>
2022-04-07 12:03:18 -0400

    Take the ball in fornt of the green line

--- Pong/game/scripting/draw_actors_action.py
+++ Pong/game/scripting/draw_actors_action.py
@@ -35,9 +35,9 @@ class DrawActorsAction(Action):
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_circle(ball)
         self._video_service.draw_rectangle(player1)
         self._video_service.draw_rectangle(player2)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages)
+        self._video_service.draw_circle(ball)
         self._video_service.flush_buffer()
